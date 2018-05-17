748fc566cd8822145c5e75ea9a3d7b8f2060a4ab
Ravinder.Singh12 <Ravinder.Singh@dal.ca>
2018-05-17 11:04:44 -0700

    second day

---
+++ part1/get_capabilities.py
@@ -0,0 +1,28 @@
+from ncclient import manager
+import sys
+
+
+HOST='10.0.131.202'
+
+PORT=830
+
+USER='cisco1'
+PASS='cisco1'
+
+
+def main():
+
+
+
+ with manager.connect(host=HOST,port=PORT,username=USER,
+             password=PASS,hostkey_verify=False,
+             device_params={'name':'default'},
+             look_for_keys=False,allow_agent=False)as m:
+	
+             print('*** Here are the Remote  Devices Capabilities***')
+	     for capability in m.server_capabilities:
+                print(capability.split('?')[0])
+
+
+if __name__=='__main__':
+ sys.exit(main())
